8ffdbe5116ef172eab3ef2f077fb58eae776fa01
Reid Weber <reid_weber@hotmail.com>
2020-01-31 15:26:45 -0800

    Collection Delegate and Core Data

---
+++ Swiftly_Project/CoreDataController.swift
@@ -0,0 +1,114 @@
+//
+//  CoreDataController.swift
+//  Swiftly_Project
+//
+//  Created by Reid Weber on 1/31/20.
+//  Copyright © 2020 Reid Weber. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+public let idKey: String = "id"
+public let dateKey: String = "createdAt"
+public let widthKey: String = "width"
+public let heightKey: String = "height"
+public let priceKey: String = "price"
+public let imageUrlKey: String = "imageUrl"
+public let originalPriceKey: String = "originalPrice"
+public let displayNameKey: String = "displayName"
+
+class CoreDataController: NSObject {
+
+    /**
+     * Save a new record with the values provided.
+     */
+    class func saveItem(_ data: NSDictionary, _ persistentContainer: NSPersistentContainer? = nil) {
+        let managedContext: NSManagedObjectContext = CoreDataController.getContext(persistentContainer)
+        let entity = NSEntityDescription.entity(forEntityName: "SpecialItem", in: managedContext)!
+        let item = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        let uuid = UUID()
+        
+        item.setValue(Date(), forKeyPath: dateKey)
+        item.setValue(uuid.uuidString, forKeyPath: idKey)
+        item.setValue(data["display_name"], forKey: displayNameKey)
+        item.setValue(data["width"], forKey: widthKey)
+        item.setValue(data["height"], forKey: heightKey)
+        item.setValue(data["imageUrl"], forKey: imageUrlKey)
+        item.setValue(data["price"], forKey: priceKey)
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    /**
+     * Fetch and return all existing records.
+     */
+    class func fetchItems(_ persistentContainer: NSPersistentContainer? = nil) -> [NSManagedObject] {
+        let managedContext: NSManagedObjectContext = CoreDataController.getContext(persistentContainer)
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "SpecialItem")
+        
+        do {
+            let templates = try managedContext.fetch(fetchRequest)
+            return templates
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+            return []
+        }
+    }
+    
+    /**
+     * Fetch and return a single record.
+     */
+    class func fetchItem(_ id: String, _ persistentContainer: NSPersistentContainer? = nil) -> NSManagedObject? {
+        let managedContext: NSManagedObjectContext = CoreDataController.getContext(persistentContainer)
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "SpecialItem")
+        fetchRequest.predicate = NSPredicate(format: "id == %@", id)
+        do {
+            let template = try managedContext.fetch(fetchRequest)
+            return template.first
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+            return nil
+        }
+    }
+    
+    /**
+     * Delete the record provided.
+     */
+    class func delete(_ template: NSManagedObject, _ persistentContainer: NSPersistentContainer? = nil) {
+        let managedContext: NSManagedObjectContext = CoreDataController.getContext(persistentContainer)
+        managedContext.delete(template)
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    /**
+     * Get the relevant NSManagedObjectContext based on whether or not we're in the
+     * test suite.
+     */
+    class func getContext(_ persistentContainer: NSPersistentContainer? = nil) -> NSManagedObjectContext {
+        /**
+         * This is used for the mocked CoreData stack in the Test Suite.
+         */
+        if (persistentContainer != nil) {
+            return persistentContainer!.viewContext
+        }
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Could not access AppDelegate from CoreDataController")
+        }
+        
+        let managedContext: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        return managedContext
+    }
+    
+}
--- Swiftly_Project/ManagerSpecialViewController.swift
+++ Swiftly_Project/ManagerSpecialViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-class ManagerSpecialViewController: UIViewController {
+class ManagerSpecialViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var specialItems: NSArray?
+    var canvasUnit: Int = 16
+    
+    private let reuseIdentifier = "SPECIAL_CELL"
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        NetworkController.fetchData { (data, error) in
+            let dataDictionary = (data as! NSDictionary)
+//            self.specialItems = (dataDictionary["managerSpecials"] as! NSArray)
+            self.canvasUnit = (dataDictionary["canvasUnit"] as! Int)
+            self.saveItems((dataDictionary["managerSpecials"] as! NSArray))
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! ManagerSpecialCollectionViewCell
+        let item = (self.specialItems![indexPath.row] as! NSDictionary)
+        cell.newPriceLabel.text = (item["price"] as! String)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.specialItems != nil) ? self.specialItems!.count : 0
     }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    func getFrame(_ item: NSDictionary) -> CGRect {
+        return CGRect(x: 20, y: 20, width: 100, height: 100)
+    }
+    
+    func saveItems(_ items: NSArray) {
+        
+    }
+    
 }
